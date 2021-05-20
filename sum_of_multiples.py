368e30153c1c9c00b32ac0b277f4f9c83af1a8de
elizabethsotomayor <elizabethrsotomayor@gmail.com>
2021-05-19 22:58:17 -0500

    refactor to pass all tests

--- sum_of_multiples.py
+++ sum_of_multiples.py
@@ -3,24 +3,21 @@ from typing import List
 
 def sum_of_multiples(limit: int, multiples: List[int]) -> int:
     """Return the sum of the multiples of unique numbers up to but not including the number."""
-    sum = 0
+    total = 0
 
-    # for num in range(limit, 0, -1):
-    #     # if num % multiples[0] == 0:
-    #     #     sum += num
-    #     #     print(sum, num)
-    #     for i in multiples:
-    #         # print(num, i)
-    #         if num % i == 0:
-    #             sum += num
+    lst = []
 
+    for num in range(limit - 1, 0, -1):
+        for i in multiples:
+            if i != 0:
+                if num % i == 0:
+                    lst.append(num)
+            else:
+                break
 
+    remove_duplicates = list(dict.fromkeys(lst))
 
-    return sum
+    for i in remove_duplicates:
+        total += i
 
-
-# print(sum_of_multiples(7, [3])) # 9
-# print(sum_of_multiples(4, [3, 5])) # 3
-# print(sum_of_multiples(20, [7, 13, 17]))  # 51
-print(sum_of_multiples(150, [5, 6, 8])) # 4419
-print(sum_of_multiples(20, [3,5])) # 78
+    return total
